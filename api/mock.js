622f7f1512de4ade7983662442e62b814d222852
Dramatic <2217595904@qq.com>
2022-07-19 14:27:30 +0800

    new project1

--- api/mock.js
+++ api/mock.js
@@ -2,7 +2,6 @@ import Mock from 'mockjs'
 
 import homeApi from './mockServerData/home'
 import userApi from './mockServerData/user'
-import permissionApi from './mockServerData/permission'
 Mock.mock(/api\/home\/getData/, homeApi.getStatisticalData)
 // Mock.mock('/api/user/getUser', userApi.getUserList)
 
@@ -11,5 +10,3 @@ Mock.mock(/api\/user\/getUser/, 'get', userApi.getUserList)
 Mock.mock(/api\/user\/del/,'get',userApi.deleteUser)
 Mock.mock(/api\/user\/edit/,'post',userApi.updateUser)
 Mock.mock(/api\/user\/add/,'post',userApi.createUser)
-// 权限相关
-Mock.mock(/api\/permission\/getMenu/, 'post', permissionApi.getMenu)
